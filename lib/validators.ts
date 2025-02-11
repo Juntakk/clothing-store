86cc73043cf8fe5041340543491ba2fd3c0f7a17
Nicolas Gauthier <nick.habashi@gmail.com>
2025-02-11 17:58:49 -0500

    test

--- lib/validators.ts
+++ lib/validators.ts
@@ -44,3 +44,18 @@ export const signUpFormSchema = z
     message: "Passwords don't match",
     path: ["confirmPassword"],
   });
+
+//Cart Schemas
+export const cartItemSchema = z.object({
+  productId: z.string().min(1, "Product required"),
+  name: z.string().min(1, "Name required"),
+  slug: z.string().min(1, "Slug required"),
+  qty: z.number().int().nonnegative("Quantity must be a positive number"),
+  image: z.string().min(1, "Image is required"),
+  price: currency,
+});
+
+export const insertCartSchema = z.object({
+  items: z.array(cartItemSchema),
+  itemsPrice: currency,
+});
