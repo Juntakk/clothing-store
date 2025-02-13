f06a17b37f56fb8f0df178b1480a107d3cd05200
Nicolas Gauthier <nick.habashi@gmail.com>
2025-02-13 12:54:59 -0500

    df

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  runtime: "edge", // You can globally set runtime to "edge"
 };
 
 export default nextConfig;
