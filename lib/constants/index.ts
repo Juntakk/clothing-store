9ebd52786213f6fad5c87db4be1a46161b7a05ad
Nicolas Gauthier <nick.habashi@gmail.com>
2025-02-21 19:52:21 -0500

    61

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -16,9 +16,9 @@ export const signUpDefaultValues = {
   confirmPassword: "",
 };
 export const shippingAddressDefault = {
-  fullName: "John Doe",
-  streetAddress: "123 Main st",
-  city: "Anytown",
-  postalCode: "12345",
-  country: "USA",
+  fullName: "",
+  streetAddress: "",
+  city: "",
+  postalCode: "",
+  country: "",
 };
