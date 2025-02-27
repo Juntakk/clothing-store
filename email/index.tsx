38dc46cb13dbe6cd132db4daa9c971d429c81536
Nicolas Gauthier <nick.habashi@gmail.com>
2025-02-27 15:46:41 -0500

    x

--- email/index.tsx
+++ email/index.tsx
@@ -2,10 +2,8 @@ import { Resend } from "resend";
 import { SENDER_EMAIL, APP_NAME } from "@/lib/constants";
 import PurchaseReceiptEmail from "./purchase-receipt";
 import { Order } from "@/types";
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-require("dotenv").config();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_DSZw53w5_RzgkfYddNHdFCemqHSUHTtfQ");
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
   try {
--- email/purchase-receipt.tsx
+++ email/purchase-receipt.tsx
@@ -112,7 +112,7 @@ export default function PurchaseReceiptEmail({ order }: OrderInformationProps) {
                       className="rounded"
                       src={
                         item.image.startsWith("/")
-                          ? `${process.env.NEXT_PUBLIC_SERVER_URL}${item.image}`
+                          ? `http://localhost:3000${item.image}`
                           : item.image
                       }
                     />
