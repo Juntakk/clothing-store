9a7c21fab7671c49c72ddc4d669bdb18f0c19db3
Nicolas Gauthier <nick.habashi@gmail.com>
2025-02-27 15:44:42 -0500

    Update index.tsx

--- email/index.tsx
+++ email/index.tsx
@@ -5,7 +5,7 @@ import { Order } from "@/types";
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require("dotenv").config();
 
-const resend = new Resend(process.env.RESEND_API_KEY as string);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
   try {
