5bc77a278aacad3171e68fa32988005729039b9e
Nicolas Gauthier <nick.habashi@gmail.com>
2025-02-27 15:06:01 -0500

    Update index.tsx

--- email/index.tsx
+++ email/index.tsx
@@ -2,6 +2,8 @@ import { Resend } from "resend";
 import { SENDER_EMAIL, APP_NAME } from "@/lib/constants";
 import PurchaseReceiptEmail from "./purchase-receipt";
 import { Order } from "@/types";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const resend = new Resend(process.env.RESEND_API_KEY as string);
 
